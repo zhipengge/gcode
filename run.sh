b573a0f0013453f026b66cf691dbeef8df97c462
zhipengge <gezhipeng0201@gmail.com>
2024-12-19 09:04:11 +0800

    update

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 # bash build.sh -a x86 -m Release -j 6
 # bash build.sh -a x86 -m Debug -j 6
 bash build.sh -a aarch64 -m Release -j 6
+# bash build.sh -a aarch64 -m Debug -j 6
 # # unit test
 # for test in $(find tests -name "*_test"); do
 #     echo "================================="
