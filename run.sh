c49aa37b251fa3107dc06864c2568243417e20f1
zhipengge <gezhipeng0201@gmail.com>
2025-01-17 19:39:19 +0800

    update

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# bash build.sh -a x86 -m Release -j 6
+bash build.sh -a x86 -m Release -j 6
 # bash build.sh -a x86 -m Debug -j 6
-bash build.sh -a aarch64 -m Release -j 6
+# bash build.sh -a aarch64 -m Release -j 6
 # bash build.sh -a aarch64 -m Debug -j 6
 # # unit test
 # for test in $(find tests -name "*_test"); do
